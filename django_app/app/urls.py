12eaef5759226dcf61ec193e0994f3b4405080c8
123@anupkumarssm <anupkumar.sasaram@gmail.com>
2025-06-28 09:42:52 +0530

    ..

--- django_app/app/urls.py
+++ django_app/app/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('home', views.homepage, name='homepage'),
+    path('homepage', views.homepage, name='homepage'),
 ]
